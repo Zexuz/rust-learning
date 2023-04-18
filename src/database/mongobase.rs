8e0e1fca0096d2c8656cad17e6fd16fbee9b6e4f
Zexuz <robin.edbom@gmail.com>
2023-04-18 18:45:18 +0100

    Random shit

--- src/database/mongobase.rs
+++ src/database/mongobase.rs
@@ -1,3 +1,5 @@
+use std::any::TypeId;
+use std::collections::HashMap;
 use mongodb::Client;
 use mongodb::options::ClientOptions;
 
@@ -7,12 +9,46 @@ pub struct MongoDbBase<T> {
     pub collection: mongodb::Collection<T>,
 }
 
+#[derive(Debug, Eq, PartialEq, Hash)]
+enum Collections {
+    Customer,
+}
+
+struct Mapper {
+    dict: HashMap<TypeId, &'static str>,
+}
+
+impl Mapper {
+    fn new() -> Self {
+        let mut dict = HashMap::new();
+        dict.insert(Collections::Orders, "orders");
+        dict.insert(Collections::Payments, "payments");
+
+        Self { dict }
+    }
+
+    fn try_get_collection_name(&self, collection: TypeId) -> Option<&&'static str> {
+        self.dict.get(&collection)
+    }
+}
+
+
 impl<T> MongoDbBase<T> {
     pub async fn new(config: &MongoDbConfig) -> Self {
         let client_options = ClientOptions::parse(config.get_connection_string()).await.unwrap();
         let client = Client::with_options(client_options).unwrap();
 
         let db = client.database(&config.db_name);
+
+        let mapper = Mapper::new();
+        let type_value = TypeId::of::<T>();
+        let collectionName =  mapper.try_get_collection_name(type_value).unwrap();
+
+        if String::is_empty(collectionName)  {
+            let errorMsg = format!("Collection name not found for {:?}", type_value);
+            panic!();
+        };
+
         let collection = db.collection::<T>("customers");
         MongoDbBase {
             client,
